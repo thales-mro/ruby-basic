ee56603d3f182b6c42f8c59bcc08887371ede60f
Thales <thalesteng@gmail.com>
2019-07-17 11:05:53 -0300

    Implemented basic control flow logic for ruby

---
+++ control_flow.rb
@@ -0,0 +1,21 @@
+var = false
+
+if var
+	puts "Do something"
+elsif var == false
+	puts "Do something else"
+else
+	puts "Not going to be executed"
+end
+
+#unless checks if statement is false
+
+unless var
+	puts "Yay"
+else
+	puts "Nope"
+end
+
+#cool stuff
+
+puts "Good to go!" unless var

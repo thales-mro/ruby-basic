392fad01a820f8a62e7ac1a0e41123977096c132
Thales Mateus Rodrigues Oliveira <ra148051@students.ic.unicamp.br>
2020-01-03 21:22:14 -0300

    Added few exercises resolution

---
+++ lrthw/ex3-4.rb
@@ -0,0 +1,22 @@
+puts "I will now count my chickens:"
+
+puts "Hens", 25 + 30/6
+puts "Roosters", 100 - 25 * 3 % 4
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2?", 3 + 2
+
+vrb = 3.0
+
+puts "This is how we can interpolate values: #{vrb}. Not that hard"
+
+intvar = 52
+
+puts "We can do good stuff like #{vrb*intvar} without problems"
---
+++ lrthw/ex5.rb
@@ -0,0 +1,13 @@
+my_name = 'Thales O.'
+my_age = 24
+my_height = 190
+my_weight = 82
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about %s." % my_name
+puts "He's %d centimeters tall." % my_height
+puts "He's %d kilograms heavy." % my_weight
+puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
+puts "His teeth are usually %s depending on the coffee" % my_teeth

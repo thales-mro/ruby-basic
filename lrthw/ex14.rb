3630993e5f07fc0af2879b963cfa00408ec53031
Thales Mateus Rodrigues Oliveira <ra148051@students.ic.unicamp.br>
2020-01-06 18:43:31 -0300

    Added exercise 14

---
+++ lrthw/ex14.rb
@@ -0,0 +1,11 @@
+user = ARGV.first
+
+prompt = '> '
+
+puts "Hi #{user}, I'm the #{$0} script."
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user}?"
+print prompt
+likes = STDIN.gets.chomp()
+
+puts likes

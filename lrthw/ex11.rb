c063e9c3af4db2e67bc83d8e52b79928a7484623
Thales Mateus Rodrigues Oliveira <ra148051@students.ic.unicamp.br>
2020-01-06 17:51:52 -0300

    Added exercises 11 to 13

---
+++ lrthw/ex11.rb
@@ -0,0 +1,4 @@
+print "How old are you? "
+age = gets.chomp()
+
+puts "So you're #{age} years old!"
---
+++ lrthw/ex13.rb
@@ -0,0 +1,9 @@
+first, second, third = ARGV
+
+puts "Let's play with argv!"
+
+puts "The script is called: #{$0}"
+
+puts "Your first variable is #{first}"
+puts "Your second variable is #{second}"
+puts "Your third variable is #{third}"
